ca6c49e7a06329c3bfb01ada42b7b7a8a6f73953
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-10-20 13:36:07 +0700

    membuat db.connect

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ const db = mysql.createConnection({
     port: 3308
 });
 
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ' + err.stack);
+    return;
+  }
+  console.log('Connection successfully');
+});
+
